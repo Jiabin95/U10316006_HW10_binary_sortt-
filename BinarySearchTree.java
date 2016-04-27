ccccde6852ccf9a5cf15dabc989bb01e807a0f08
Jiabin95 <ongjiabin@gmail.com>
2016-04-27 21:50:54 +0800

    Create Inorder Postorder Preorder method

---
+++ BinarySearchTree.java
@@ -0,0 +1,110 @@
+class Node<T> {
+//The value present in the node
+   public int value;
+ 
+  //Left subtree
+  public Node left;
+
+  //Right subtree
+  public Node right;
+
+  public Node(int value) {
+    this.value = value;
+  }
+}
+
+public class BinarySearchTree {
+	//Refrence for the root of the tree
+	public Node root;
+	public BinarySearchTree insert(int value) {
+		//New a node constructor  
+		Node node = new Node<>(value);
+		if (root == null) {
+			root = node;
+			return this;
+		}
+		insertRec(root, node);
+		return this;
+		}
+  
+	private void insertRec(Node latestRoot, Node node) {
+		if (latestRoot.value > node.value) {
+			if (latestRoot.left == null) {
+				latestRoot.left = node;
+				return;
+				}		 
+				else {
+					insertRec(latestRoot.left, node);
+				}
+		}	 
+		else {
+			if (latestRoot.right == null) {
+				latestRoot.right = node;
+				return;
+			} 
+			else {
+				insertRec(latestRoot.right, node);
+			}
+		}
+	}
+  
+//Printing the contents of the tree in an inorder way
+public void printInorder() {
+	printInOrderRec(root);
+    System.out.println("");
+}
+  
+//Recursively print the contents in an inorder way
+private void printInOrderRec(Node currRoot) {
+	if (currRoot == null) {
+		return;
+    }
+
+    printInOrderRec(currRoot.left);
+
+    System.out.print(currRoot.value + ", ");
+
+    printInOrderRec(currRoot.right);
+
+}
+  
+//Printing the contents of the tree in a Preorder way.
+public void printPreorder() {
+	printPreOrderRec(root);
+    System.out.println("");
+}
+
+//Recursively print the contents in a Preorder way
+private void printPreOrderRec(Node currRoot) {
+    if (currRoot == null) {
+      return;
+    }
+	
+    System.out.print(currRoot.value + ", ");
+
+    printPreOrderRec(currRoot.left);
+
+    printPreOrderRec(currRoot.right);
+}
+
+//Printing the contents of the tree in a Postorder way
+public void printPostorder() {
+	printPostOrderRec(root);
+    System.out.println("");
+}
+  
+//Recursively print the contents in a Postorder way
+private void printPostOrderRec(Node currRoot) {
+	if (currRoot == null) {
+		return;
+    }
+
+    printPostOrderRec(currRoot.left);
+
+    printPostOrderRec(currRoot.right);
+
+    System.out.print(currRoot.value + ", ");
+
+  }
+
+}
